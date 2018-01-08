2513669c0167cd84f17793a85d89955dd296b884
FuryAdcom <fastidiosowindows@hotmail.com>
2018-01-08 15:59:02 -0400

    Score

--- core/src/com/example/jose/lapsproyect/Laps.java
+++ core/src/com/example/jose/lapsproyect/Laps.java
@@ -10,12 +10,15 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 
 import java.awt.Dimension;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Label;
 import java.awt.Toolkit;
 
 public class Laps extends ApplicationAdapter {
@@ -24,11 +27,18 @@ public class Laps extends ApplicationAdapter {
 	public static Texture back;
 	private SpriteBatch batch;
 
+    public FreeTypeFontGenerator generator;
+    public BitmapFont score;
 
 	@Override
 	public void create () {
 		circles = new TextureAtlas(Gdx.files.internal("fichas.atlas"));
 		back = new Texture(Gdx.files.internal("Back.png"));
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/AbcariCircle Wide.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parametro = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        parametro.size = 22;
+        score = generator.generateFont(parametro);
 		batch = new SpriteBatch();
 
 		OrthographicCamera cam = new OrthographicCamera();
@@ -43,7 +53,9 @@ public class Laps extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(back, 0 , 0, Gdx.graphics.getWidth()+1, Gdx.graphics.getHeight()+1);
-		batch.end();
+		score.draw(batch, "SCORE", Gdx.graphics.getWidth()/2 - 120, Gdx.graphics.getHeight()-110);
+		score.getData().setScale(2f, 0.9f);
+        batch.end();
 		tablero.act();
 		tablero.draw();
 	}
@@ -51,6 +63,7 @@ public class Laps extends ApplicationAdapter {
 	@Override
 	public void resize(int width, int height) {
 		tablero.getViewport().update(width,height,true);
+
 	}
 	
 	@Override
@@ -58,5 +71,6 @@ public class Laps extends ApplicationAdapter {
 		tablero.dispose();
 		circles.dispose();
 		back.dispose();
+		score.dispose();
 	}
 }
